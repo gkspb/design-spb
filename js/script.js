c0e2d723ed92410905a458cb0916d466555274cc
Vadim_Tsarev <vadimjke.tsar@gmail.com>
2023-08-08 13:40:07 +0300

    edits from the designer

--- js/script.js
+++ js/script.js
@@ -67,43 +67,43 @@ $(function () {
         updateHash: true,
         touchScroll: true,
         updateHash: false,
-        // after: function (index, sections) {
-        //     function updateStyles() {
-        //         let targetBlockClass = "target-block";
-		// 		// && window.innerHeight >= 800
-        //         if (sections[index].hasClass(targetBlockClass)) {
-        //             $(".header").addClass("header_small-padding");
-        //             $(".header-wrapper").addClass("header_small-margin");
-        //             if ($('.mobile-nav_bottom').hasClass('mobile-nav_bottom-deactive')) {
-        //                 $(".header-text").addClass("header_black");
-        //                 $(".menu-item").addClass("menu-item_black");
-        //                 $(".menu-burger").addClass("menu-burger_black");
-        //                 $(".icon-white").addClass("none");
-        //                 $(".icon-black").removeClass("none");
-        //             } else {
-        //                 $(".header-text").removeClass("header_black");
-        //                 $(".menu-item").removeClass("menu-item_black");
-        //                 $(".menu-burger").removeClass("menu-burger_black");
-        //             }
-		// 			// if (window.innerHeight >= 800)
-        //         } else {
-        //             $(".header-text").removeClass("header_black");
-        //             $(".icon-white").removeClass("none");
-        //             $(".icon-black").addClass("none");
-        //             $(".header").removeClass("header_small-padding");
-        //             $(".header-wrapper").removeClass("header_small-margin");
-        //             $(".menu-burger").removeClass("menu-burger_black");
-        //             $(".menu-item").removeClass("menu-item_black");
-        //         }
-        //     }
-        //     updateStyles();
-        //     $(window).on('scroll', function () {
-        //         updateStyles();
-        //     });
-        //     $('#menu-toggle').on('click', function () {
-        //         updateStyles();
-        //     });
-        // },
+        after: function (index, sections) {
+            function updateStyles() {
+                let targetBlockClass = "target-block";
+				// && window.innerHeight >= 800
+                if (sections[index].hasClass(targetBlockClass)) {
+                    $(".header").addClass("header_small-padding");
+                    $(".header-wrapper").addClass("header_small-margin");
+                    if ($('.mobile-nav_bottom').hasClass('mobile-nav_bottom-deactive')) {
+                        $(".header-text").addClass("header_black");
+                        $(".menu-item").addClass("menu-item_black");
+                        $(".menu-burger").addClass("menu-burger_black");
+                        $(".icon-white").addClass("none");
+                        $(".icon-black").removeClass("none");
+                    } else {
+                        $(".header-text").removeClass("header_black");
+                        $(".menu-item").removeClass("menu-item_black");
+                        $(".menu-burger").removeClass("menu-burger_black");
+                    }
+					// if (window.innerHeight >= 800)
+                } else {
+                    $(".header-text").removeClass("header_black");
+                    $(".icon-white").removeClass("none");
+                    $(".icon-black").addClass("none");
+                    $(".header").removeClass("header_small-padding");
+                    $(".header-wrapper").removeClass("header_small-margin");
+                    $(".menu-burger").removeClass("menu-burger_black");
+                    $(".menu-item").removeClass("menu-item_black");
+                }
+            }
+            updateStyles();
+            $(window).on('scroll', function () {
+                updateStyles();
+            });
+            $('#menu-toggle').on('click', function () {
+                updateStyles();
+            });
+        },
     });
 });
 
