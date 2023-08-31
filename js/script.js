dfe44e8ab949f4c70700d853b0780a85d5c5a4b2
Vadim_Tsarev <vadimjke.tsar@gmail.com>
2023-08-31 16:27:18 +0300

    edit block before/after

--- js/script.js
+++ js/script.js
@@ -82,10 +82,6 @@ let swiper2 = new Swiper(".mySwiper2", {
 
 let swiper3 = new Swiper(".mySwiper3", {
 	slidesPerView: 2,
-	autoplay: {
-		delay: 4000,
-		disableOnInteraction: false,
-	},
 	spaceBetween: 20,
 	pagination: {
 		el: ".swiper-pagination",
@@ -115,9 +111,6 @@ let swiper5 = new Swiper(".mySwiper5", {
 	breakpoints: {
 		250: {
 			slidesPerView: 1
-		},
-		650: {
-			slidesPerView: 2
 		},
 		767: {
 			slidesPerView: 3
@@ -274,7 +267,7 @@ if (beforeButton && afterButton && imgElement) {
 //убрать блюр
 function removeBlur(element) {
 	element.removeEventListener("mouseover", removeBlur);
-	var blurElement = element.querySelector(".item-blur");
+	let blurElement = element.querySelector(".item-blur");
 	if (blurElement) {
 		blurElement.style.opacity = "0";
 		setTimeout(function () {
@@ -312,3 +305,43 @@ burgerItemCheckboxes.forEach(checkbox => {
         }
     });
 });
+
+// хлебные крошки
+// var currentPath = window.location.pathname;
+// var urlSegments = currentPath.split('/').filter(segment => segment.trim() !== '');
+// var breadcrumbsHTML = '<a href="/">Главная</a> /';
+// for (var i = 0; i < urlSegments.length; i++) {
+//     currentPath += urlSegments[i] + '/';
+//     breadcrumbsHTML += ' <a href="' + currentPath + '"' + urlSegments[i] + '</a>';
+// }
+// document.getElementById('breadcrumbs').innerHTML = breadcrumbsHTML;
+
+function imageComparison(selector) {
+    let comparison = $(selector)
+        .addClass("image-comparison")
+        .prepend('<div class="image-comparison__before"></div>')
+        .append('<button class="image-comparison__slider"></button>');
+    let images = comparison
+        .find("img")
+        .addClass("image-comparison__image")
+        .css("max-width", comparison.width());
+    let before = comparison
+        .find(".image-comparison__before")
+        .append(images.eq(0));
+    comparison
+        .find(".image-comparison__slider")
+        .on("dragstart", () => false) 
+        .on("mousedown", function(e) {
+            let slider = $(this);
+            let doc = $(document).on("mousemove", (e) => {
+                let offset = e.pageX - comparison.position().left;
+                let width = comparison.width();
+                if (offset < 0) offset = 0;
+                if (offset > width) offset = width;
+                slider.css("left", offset + "px");
+                before.css("width", offset + "px");
+            });
+            doc.on("mouseup", () => doc.off("mousemove"));
+        });
+};
+imageComparison("#image-comparison");
